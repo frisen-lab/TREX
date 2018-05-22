c0262ee73accff727a5c8ff0432527c810f8518b
Marcel Martin <marcel.martin@scilifelab.se>
2018-05-22 16:10:21 +0200

    A setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jfrisen1801',
+    version='0.1',
+    author='',
+    author_email='',
+    url='',
+    description='',
+    long_description='',
+    license='',
+    py_modules=['180425_lineage_loom'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        #'License :: ...',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ]
+)
