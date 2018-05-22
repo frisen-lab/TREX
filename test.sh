61ca1acabc1acf6203a355ad96e03f6f5e0ecea4
Marcel Martin <marcel.martin@scilifelab.se>
2018-05-22 22:02:56 +0200

    Fix test script

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 rm -r lineage_run
-python3 180425_lineage_loom.py -p mini2 -gn hg38_EGFP-30N -chr chrEGFP-30N
+python3 180425_lineage_loom.py -p mini -gn hg38_EGFP-30N -chr chrEGFP-30N
 diff -ur mini-expected lineage_run && echo "Test ok" || ( echo "Test fail"; exit 1)
