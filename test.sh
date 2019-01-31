e25f4cfe11fb7a02e5005ff7e5bb061356c73a60
Marcel Martin <marcel.martin@scilifelab.se>
2019-01-31 13:05:00 +0100

    Test also loom file generation
    Also add some dummy data to make this work

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
-braintrace --delete --no-plot -s 695 -e 724 mini
-diff -ur mini-expected lineage_run && echo "Test ok" || ( echo "Test fail"; exit 1)
+braintrace --delete --loom --no-plot -s 695 -e 724 tests/data/outs
+diff -ur -xdata.loom tests/expected lineage_run && echo "Test ok" || ( echo "Test fail"; exit 1)
