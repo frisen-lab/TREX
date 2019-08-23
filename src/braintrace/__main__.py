218d143d6c81ae67d34d9f774c15ad41bc81c3a5
Marcel Martin <marcel.martin@scilifelab.se>
2019-08-23 13:05:20 +0200

    Move DatasetReader to separate module

--- src/braintrace/__main__.py
+++ src/braintrace/__main__.py
@@ -9,6 +9,7 @@ import warnings
 import logging
 from pathlib import Path
 from collections import Counter
+
 from typing import List, Dict, Iterable
 
 from alignlib import hamming_distance
@@ -21,19 +22,17 @@ with warnings.catch_warnings():
 from . import __version__
 from .cellranger import make_cellranger, CellRangerError
 from .utils import NiceFormatter
-from .bam import read_bam
 from .clustering import cluster_sequences
 from .clone import CloneGraph
 from .molecule import Molecule, compute_molecules
 from .cell import Cell, compute_cells
+from .error import BraintraceError
+from .dataset import DatasetReader
+
 
 __author__ = 'leonie.von.berlin@ki.se'
 
 logger = logging.getLogger(__name__)
-
-
-class BraintraceError(Exception):
-    pass
 
 
 def main():
@@ -296,73 +295,6 @@ def run_braintrace(
             logger.warning("Writing a loom file only for the first transcriptome dataset")
         cellranger = make_cellranger(transcriptome_inputs[0])
         write_loom(cells, cellranger, output_dir, clone_id_length=end - start)
-
-
-class DatasetReader:
-    def __init__(self, output_dir: Path, genome_name, chromosome, start, end, prefix: bool):
-        self.output_dir = output_dir
-        self.genome_name = genome_name
-        self.chromosome = chromosome
-        self.start = start
-        self.end = end
-        self.prefix = prefix
-
-    def read_one(self, path, output_bam_path):
-        cellranger_dir = make_cellranger(path, self.genome_name)
-        allowed_cell_ids = cellranger_dir.cellids()
-        reads = read_bam(
-            cellranger_dir.bam, output_bam_path, allowed_cell_ids, self.chromosome, self.start, self.end)
-        assert len(reads) > 0, "No reads"
-        return reads
-
-    def read_all(self, transcriptome_inputs, amplicon_inputs, names, allowed_cell_ids):
-        n_transcriptome = len(transcriptome_inputs)
-        n_amplicon = len(amplicon_inputs)
-        assert n_transcriptome > 0
-        assert n_amplicon == 0 or n_amplicon == n_transcriptome
-        assert n_transcriptome == len(names)
-
-        # Cases:
-        # - 1 transcriptome, 0 amplicon -- do not modify cell ids
-        # - 1 transcriptome, 1 amplicon -- modify cell ids?
-        # - n transcriptome, 0 amplicon -- modify cell ids!
-        # - n transcriptome, n amplicon -- modify cell ids!
-
-        if n_transcriptome == 1 and n_amplicon == 0:
-            reads = self.read_one(transcriptome_inputs[0], self.output_dir / "entries.bam")
-        elif n_transcriptome > 1 and n_amplicon == 0:
-            datasets = []
-            for path, name in zip(transcriptome_inputs, names):
-                assert name is not None
-                datasets.append(
-                    self.read_one(path, self.output_dir / (name + "_entries.bam"))
-                )
-            reads = self.merge_datasets(datasets, names)
-        else:
-            assert n_amplicon > 0
-            raise BraintraceError("Reading amplicon data not supported at the moment")
-
-        if allowed_cell_ids:
-            logger.debug("Allowed cell ids:\n- %s\n  ...", "\n- ".join(list(allowed_cell_ids)[:10]))
-            logger.debug("Cell ids of reads:\n- %s\n  ...", "\n- ".join(r.cell_id for r in reads[:10]))
-            reads = [r for r in reads if r.cell_id in allowed_cell_ids]
-        if not reads:
-            raise BraintraceError("No reads left after --filter-cellids filtering")
-        return reads
-
-    def merge_datasets(self, datasets, names):
-        if self.prefix:
-            def combine(cell_id, affix):
-                return affix + "_" + cell_id
-        else:
-            def combine(cell_id, affix):
-                return cell_id + "_" + affix
-        reads = []
-        for dataset, name in zip(datasets, names):
-            for read in dataset:
-                reads.append(read._replace(cell_id=combine(read.cell_id, name)))
-
-        return reads
 
 
 def read_allowed_cellids(path):
---
+++ src/braintrace/error.py
@@ -0,0 +1,2 @@
+class BraintraceError(Exception):
+    pass
