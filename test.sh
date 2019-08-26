9b0027ccd70b7dd7da7b50208e3029bfa7923c8e
Marcel Martin <marcel.martin@scilifelab.se>
2019-08-26 13:05:57 +0200

    Use braintrace_run for tests

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 pytest tests
-braintrace -o lineage_run --delete --loom --umi-matrix -s 695 -e 724 tests/data/
-diff -ur -xdata.loom tests/expected lineage_run && echo "Test ok" || ( echo "Test fail"; exit 1)
+braintrace --delete --loom --umi-matrix -s 695 -e 724 tests/data/
+diff -ur -xdata.loom tests/expected braintrace_run && echo "Test ok" || ( echo "Test fail"; exit 1)
