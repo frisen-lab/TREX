20f64f50c26703577051c39442e420f2c82d8704
Marcel Martin <marcel.martin@scilifelab.se>
2019-08-27 10:34:37 +0200

    Ignore version when comparing logs

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 set -euo pipefail
 pytest tests
 braintrace --delete --loom --umi-matrix -s 695 -e 724 tests/data/
-diff -ur -xdata.loom tests/expected braintrace_run && echo "Test ok" || ( echo "Test fail"; exit 1)
+diff -ur -xdata.loom -xlog.txt tests/expected braintrace_run
+diff <(sed 1d tests/expected/log.txt) <(sed 1d braintrace_run/log.txt)
