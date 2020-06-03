2a0bb5b60e2a29fd1eb61f9969bab3892e177487
leonievb <leonie@vonberlin.de>
2020-06-03 10:21:34 +0200

    Added automatic testing of the smartseq3 subcommand by adding a bam file of a small (downsampling to 0.5%) HEK in vitro data set, expected output files and an adapted testing code

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-set -euo pipefail
-pytest tests
+set -euo pipefail pytest tests
 braintrace run10x --delete --loom --umi-matrix -s 695 -e 724 tests/data/
 diff -ur -xdata.loom -xlog.txt tests/expected braintrace_run
 diff <(sed 1d tests/expected/log.txt) <(sed 1d braintrace_run/log.txt)
+braintrace smartseq3 --delete --chr EGFP-30N --output braintrace_smartseq3_run --start 2330 --end 2359 --read-matrix --readcount-threshold 1 tests/data/smartseq3_test.bam
+diff <(sed 1d tests/expected_smartseq3/log.txt) <(sed 1d braintrace_smartseq3_run/log.txt)
