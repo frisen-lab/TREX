f5264802dab246aa5050abd973df62324e1e193b
leonievb <leonie@vonberlin.de>
2020-06-03 10:21:34 +0200

    Added automatic testing of the smartseq3 subcommand by adding a bam file of a small (downsampling to 0.5%) HEK in vitro data set, expected output files and an adapted testing code

--- test.sh
+++ test.sh
@@ -4,3 +4,6 @@ pytest tests
 trex run10x --delete --loom --umi-matrix -s 695 -e 724 tests/data/
 diff -ur -xdata.loom -xlog.txt tests/expected trex_run
 diff -u <(sed 1d tests/expected/log.txt) <(sed 1d trex_run/log.txt)
+
+trex smartseq3 --delete --chr EGFP-30N --output trex_smartseq3_run --start 2330 --end 2359 --read-matrix --readcount-threshold 1 tests/data/smartseq3_test.bam
+diff <(sed 1d tests/expected_smartseq3/log.txt) <(sed 1d trex_smartseq3_run/log.txt)
