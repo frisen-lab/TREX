6b802b9d6074e576797d1aea4b419ec215136a2d
Marcel Martin <marcel.martin@scilifelab.se>
2023-10-26 19:26:25 +0200

    More tests

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -5,5 +5,7 @@ def test_is_low_complexity():
     assert is_low_complexity("AAAAAAAAAAAAAAAAAAAAAAAAAAGAAA")
     assert is_low_complexity("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
     assert is_low_complexity("AAAAA-------------------------")
+    assert is_low_complexity("AAAAAAAAAAAAAAAAAAAAAAATTTCATA")
     assert not is_low_complexity("ACGTACGTACGTACGTACGTACGTACGTAC")
     assert not is_low_complexity("ACGTACGTGGGGGGGGGGGGGGGGGGGGGGG")
+    assert not is_low_complexity("AAGGGGGAAGCAAGAAAATGGCCAAGGGAA")
