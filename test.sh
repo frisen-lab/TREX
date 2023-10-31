bf70276da112e872f6d920d35718251a3609c20d
Agustin Corbat <agustin.corbat@ki.se>
2023-10-31 10:40:46 +0100

    update tests as files were moved and parameters updated

--- test.sh
+++ test.sh
@@ -26,5 +26,5 @@ trex run10x --per-cell --delete -s 695 -e 724 -o trex_per_cell tests/data
 diff -u <(sed 1d tests/expected_per_cell/log.txt) <(sed 1d trex_per_cell/log.txt)
 
 # Ensure --filter-clone-id works
-trex run10x --per-cell --delete -s 695 -e 724 -o trex_filter_clone_id -b tests/data/blacklist.csv tests/data
+trex run10x --per-cell --delete -s 695 -e 724 -o trex_filter_clone_id --filter-cloneids tests/data/exclude_list.csv tests/data
 diff -u <(sed 1d tests/expected_filter_clone_id/log.txt) <(sed 1d trex_filter_clone_id/log.txt)
