frisen-lab/TREX

commit aa145a95ffa45c4bd026adf712d1ba1fdea32733

Faster processing of the exclusion list
In summary, processing a long exclusion list (100000 entries) now takes a
couple of seconds instead of days.

Previously, we would compare every clone id to every clone id in the
exclusion list. Each comparison uses the is_similar() function, which
implements the comparison by looking at each character of the clone IDs in
turn and comparing them.

So if we have *m* detected clone IDs, *n* clone IDs in the exclusion list
and clone IDs have a length of *k*, runtime would be O(mnk).

I initially thought that the comparison to each excluded clone ID is necessary
because is_similar allows for approximate matches. However, it turns out
that is_similar is called with max_hamming=0, that is, only exact matches
are allowed anyway, so it is equivalent to just look up each detected clone
ID in a set that contains the excluded clone IDs. Runtime becomes
effectively O(m) (kind of, if we consider the C-level operations to take
constant time).

src/trex/cli/run10x.py
@@ -248,7 +248,7 @@ def run_trex(
         corrected_molecules = [
             molecule
             for molecule in corrected_molecules
-            if not is_similar_to_any(molecule.clone_id, excluded_clone_ids)
+            if molecule.clone_id not in excluded_clone_ids
         ]
         clone_ids = [
             m.clone_id

commit 8c2747ffeca1ff7bc6f489f0af6b8c688b2b274a

Allow "partial" matches to the exclusion list
This fixes a regression introduced in
aa145a95ffa45c4bd026adf712d1ba1fdea32733:
If the clone ID that we look up in the exclusion list contains "-" or "0"
characters, we need to check only the other characters. This is now handled
by introducing a SimilaritySet class that uses exact lookups as long as
there are no '-' or '0', but falls back to the old algorithm otherwise.

src/trex/cli/run10x.py
@@ -245,10 +245,11 @@ def run_trex(
     )
 
     if excluded_clone_ids is not None:
+        similarity_set = SimilaritySet(excluded_clone_ids)
         corrected_molecules = [
             molecule
             for molecule in corrected_molecules
-            if molecule.clone_id not in excluded_clone_ids
+            if not similarity_set.contains(molecule.clone_id)
         ]
         clone_ids = [
             m.clone_id
@@ -397,14 +398,37 @@ def is_similar(s: str, t: str, min_overlap: int, max_hamming: int) -> bool:
     # TODO allowed Hamming distance should be reduced relative to the overlap length
 
 
-def is_similar_to_any(
-    s: str, exclusion_list: List[str], min_overlap: int = 0, max_hamming: int = 0
-) -> bool:
-    """Check if s is similar to any sequence in the exclusion list"""
-    for t in exclusion_list:
-        if is_similar(s, t, min_overlap, max_hamming):
+class SimilaritySet:
+    def __init__(self, strings: set[str]):
+        self._length = len(next(iter(strings)))
+        if not all(len(s) == self._length for s in strings):
+            raise ValueError("All strings must have the same length")
+        self._exact = {s for s in strings if "0" not in s and "-" not in s}
+        self._partial = [s for s in strings if "0" in s or "-" in s]
+
+    def _is_similar(self, s, t):
+        for ch1, ch2 in zip(s, t):
+            if ch1 == "-" or ch1 == "0" or ch2 == "-" or ch2 == "0":
+                continue
+            if ch1 != ch2:
+                return False
+        return True
+
+    def _occurs_partially(self, s: str) -> bool:
+        for t in self._exact:
+            if self._is_similar(s, t):
+                return True
+        return False
+
+    def contains(self, s: str) -> bool:
+        if s in self._exact:
             return True
-    return False
+        for t in self._partial:
+            if is_similar(s, t, 0, 0):
+                return True
+        if ("0" in s or "-" in s) and self._occurs_partially(s):
+            return True
+        return False
 
 
 def correct_clone_ids(

tests/test_similar.py
@@ -1,4 +1,4 @@
-from trex.cli.run10x import is_similar, is_similar_to_any
+from trex.cli.run10x import is_similar, SimilaritySet
 
 import pytest
 
@@ -64,5 +64,6 @@ def test_is_similar(s, t, similar):
         ("AAAAAAAAAA", ["TTTTTTTAAA"], False),
     ],
 )
-def test_is_similar_to_any(s, strings, similar):
-    assert is_similar_to_any(s, strings) == similar
+def test_similarity_set(s, strings, similar):
+    similarity_set = SimilaritySet(strings)
+    assert similarity_set.contains(s) == similar